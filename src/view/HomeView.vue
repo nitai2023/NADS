91b09e8f4bbb3c319ef7cdfa5f177d7df608121a
admin <3251736481@qq.com>
2024-04-13 16:05:31 +0800

    feat:登录页面

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,20 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+let username = ref("");
+let password = ref("");
+let label = ref("");
+</script>
 <template>
-  <div class="mb-4">
-    <el-button plain>Plain</el-button>
-    <el-button type="primary" plain>Primary</el-button>
-    <el-button type="success" plain>Success</el-button>
-    <el-button type="info" plain>Info</el-button>
-    <el-button type="warning" plain>Warning</el-button>
-    <el-button type="danger" plain>Danger</el-button>
+  <div id="border">
+    <el-form
+      style="max-width: 600px"
+      status-icon
+      label-width="auto"
+      class="demo-ruleForm"
+    >
+      <el-form-item>
+        <h1>网络资产探测系统</h1>
+      </el-form-item>
+      <el-form-item label="用户名">
+        <el-input v-model="username" />
+      </el-form-item>
+      <el-form-item label="密码" prop="checkPass">
+        <el-input type="password" autocomplete="off" v-model="password" />
+      </el-form-item>
+      <el-form-item label="验证码" prop="age">
+        <el-input v-model="label" style="width: 140px" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="success">登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
-
-  <el-text class="mx-1">Default</el-text>
-  <el-text class="mx-1" type="primary">Primary</el-text>
-  <el-text class="mx-1" type="success">Success</el-text>
-  <el-text class="mx-1" type="info">Info</el-text>
-  <el-text class="mx-1" type="warning">Warning</el-text>
-  <el-text class="mx-1" type="danger">Danger</el-text>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+#border {
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 100px;
+}
+#border h1 {
+  color: green;
+}
+</style>
