ad29b70b527b2a04fd86cdae971188c8ed088033
Dawson <13419524+dawson68@user.noreply.gitee.com>
2024-04-17 23:31:40 +0800

    优化用户登录和用户管理功能，修复bug并新增请求错误提醒。

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import { createPinia } from "pinia";
-import { createApp } from "vue";
+import { createApp, h } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./style.css";
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const pinia = createPinia();
 
-const app = createApp(App);
+const app = createApp({
+  render: () => h(App),
+});
+
 app.use(router).use(pinia).use(ElementPlus).mount("#app");
+
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-  }
+  app.component(key, component);
+}
